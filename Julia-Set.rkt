07465965429e0adce35f87522f31739293db7937
hebr3 <brook057@cougars.csusm.edu>
2017-01-11 16:04:14 -0800

    Still trying to save flomap->bitmap

--- Julia-Set.rkt
+++ Julia-Set.rkt
@@ -1,12 +1,14 @@
+;;;;;;;;;1;;;;;;;;;2;;;;;;;;;3;;;;;;;;;4;;;;;;;;;5;;;;;;;;;6;;;;;;;;;7;;
 #lang typed/racket
 (require math/flonum
-         images/flomap)
+         images/flomap
+         typed/racket/draw)
 
 ;; CONSTANTS
-(define WIDTH 1000)
+(define WIDTH 500)
 (define HEIGHT WIDTH)
 (define DEPTH 64)
-(define SCALE (/ WIDTH))
+(define SCALE (fl (/ 1 WIDTH)))
 
 ;; PROCEDURES
 (: julia (-> Float-Complex Float-Complex Natural))
@@ -24,7 +26,7 @@
   (: scale (-> Integer Float))
   (define (scale n)
     (- (* 3.0 SCALE (fl n)) 1.5))
-  (let ([j (julia (make-flrectangular (- (scale x)) (scale y)) z)])
+  (let ([j (julia (make-flrectangular (scale x) (scale y)) z)])
     (vector (if (= j DEPTH) 1.0 (fl (- 1 (/ j DEPTH))))
             (if (= j DEPTH) 0.0 (fl (/ j DEPTH)))
             0.0
@@ -36,5 +38,25 @@
                  (λ (x y)
                    (julia-vector x y z))))
 
-(define julia-fm (julia-img -0.8+0.156i))
-(flomap->bitmap julia-fm)
+(: julia-fm flomap)
+(define julia-fm (time (julia-img -0.8+0.156i)))
+;(time (send (flomap->bitmap (flomap-scale julia-fm 1/4))
+;             save-file "julia.png" 'png))
+(time (flomap->bitmap julia-fm))
+
+
+
+(: scene (Listof (List Integer Integer)))
+(define scene
+  (for*/list : (Listof (List Integer Integer))
+    ([x (range 4)] [y (range 3)])
+    (list x y)))
+
+(: scale-scene (-> (List Integer Integer) (List Float Float)))
+(define (scale-scene x)
+  (list (fl (- (* SCALE (first x)) 2.0))
+        (fl (- (* SCALE (second x)) 1.0))))
+
+(: scaled-scene (Listof (List Float Float)))
+(define scaled-scene
+  (map scale-scene scene))
